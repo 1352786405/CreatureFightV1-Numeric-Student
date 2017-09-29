9961e3e8032fb525583f2b5a0492a64018cd1444
haochen <1352786405@qq.com>
2017-09-29 15:01:10 -0500

    complete

--- Creature.java
+++ Creature.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Creature here.
+ * Write a description of class creature here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -9,31 +9,51 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Creature extends Actor
 {
     
+    private int playerOwnership;
+    private HealthBar creatureBar;
+    private int HealthNumber;
     /**
-     * Default constructor for objects of the Creature class
-     * 
-     * @param There are no parameters
-     * @return an object of the Creature class
+     * Act - do whatever the creature wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public Creature()
     {
-
-    }
-
+        HealthNumber = 500;
+        playerOwnership = 0;
+        creatureBar = new HealthBar(HealthNumber, HealthNumber, 10);
+        
+        
+        
+    } 
     /**
-     * Constructor that allows customization of objects of the Creature class
+     * *constructor that allows customization o f objects of the creature class
      * 
      * @param health is the amount of health the Creature object will have
      * @param whichPlayer discusses whether the creature belongs to player 1 or player 2
      * @return an object of the Creature class
      */
     public Creature( int health, int whichPlayer )
+    
+    
+    
     {
-
+        HealthNumber = health;
+        playerOwnership = whichPlayer;
+        creatureBar = new HealthBar( HealthNumber, HealthNumber, 10);
+    }
+    
+    protected HealthBar getHealthBar()
+    {
+        return creatureBar;
+    }
+    
+    public int getPlayerOwnership()
+    {
+        return playerOwnership;
     }
     
     /**
-     * attack is the code that is run when the Creature attacks its enemy
+     * attack is the code that is run when the creature attacks its enemy
      * 
      * @param There are no parameters
      * @return Nothing is returned
@@ -42,7 +62,6 @@ public class Creature extends Actor
     {
         //empty method that will get overriden in subclasses
     }
-
     /**
      * act will complete actions that the Creature object should
      * accomplish while the scenario is running
@@ -50,9 +69,8 @@ public class Creature extends Actor
      * @param There are no parameters
      * @return Nothing is returned
      */
-    public void act() 
+    public void act ()
     {
         //empty method that will get overriden in subclasses
-    }   
-
+    }
 }
